546e6ca5a2397b21e1e3cc8b7f6d5f22b9725934
Aleksander <turk.aleksander@gmail.com>
2024-11-21 14:52:18 +0100

    Updated Dockerfile build - should always download latest from repo

--- Docker_build_template.sh
+++ Docker_build_template.sh
@@ -3,4 +3,5 @@
 cd /path/to/dockerfile_directory
 #Build the Docker image, don't forget to specify the name of the image here and later when running the container
 #The dot (.) at the end of the command is important, it tells Docker to look for the Dockerfile in the current directory
-docker build -t docker_img_integ -f Dockerfile_integration .
+#Added build argument so the Dockerfile always downloads newest Git repository
+docker build --build-arg CACHEBUST=$(date +%s) -t docker_img_integ -f Dockerfile_integration .
