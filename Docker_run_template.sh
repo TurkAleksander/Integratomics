d1549d51f50d8121f4eeb1e7e92d7014dd4f99ed
Aleksander Turk <131336189+TurkAleksander@users.noreply.github.com>
2026-04-24 10:03:50 +0200

    Update directory paths

--- Docker_run_template.sh
+++ Docker_run_template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #You may need to run as sudo if you're experiencing problems with permissions
-docker run --rm -v /scratch/PROJECTS/KT_obesity_integ/For_integ:/input -v /scratch/PROJECTS/KT_obesity_integ/Integ_output:/output docker_img_integ /input /output
+docker run --rm -v /some/input/dir:/input -v /some/output/dir:/output docker_img_integ /input /output
